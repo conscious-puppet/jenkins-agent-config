380b399db332fa1d0317ac55483300bf8968a519
Abhishek Singh <abhishek17513@gmail.com>
2026-03-06 17:26:24 +0530

    fix the init command

--- flake.nix
+++ flake.nix
@@ -20,10 +20,11 @@
             Type=simple
             EnvironmentFile=%h/.config/jenkins-agent.env
             WorkingDirectory=@JENKINS_AGENT_WORK_DIR@
-            ExecStart=${pkgs.openjdk}/bin/java -jar @JENKINS_AGENT_PATH@ \
+            ExecStart=${pkgs.openjdk}/bin/java \
               -Dhudson.remoting.Launcher.pingIntervalSec=60 \
               -Dhudson.remoting.Launcher.pingTimeoutSec=120 \
               -Dorg.jenkinsci.remoting.engine.JnlpProtocol3.idleTimeout=600 \
+              -jar @JENKINS_AGENT_PATH@ \
               -url @JENKINS_URL@ \
               -secret @SECRET_KEY@ \
               -name @JENKINS_AGENT_NAME@ \
